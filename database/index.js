aaf8f8dda48192bf0372840cfecf37cde4fc871d
Ricardo Figueroa <rfigueror@gmail.com>
2018-07-13 16:04:40 -0500

    db semiworking

--- database/index.js
+++ database/index.js
@@ -1,16 +1,38 @@
 const mongoose = require('mongoose');
+//const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
+var db = mongoose.connection;
+db.on('error', console.log.bind(console, 'connection error'));
+db.once('open', () =>{
 
-let Repo = mongoose.model('Repo', repoSchema);
+  console.log('connected');
+  let repoSchema = mongoose.Schema({
+    html_url: String
+  })
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+  //repoSchema.plugin(AutoIncrement, {inc_field: 'id'});
+  let Repo = mongoose.model('Repo', repoSchema);
 
-module.exports.save = save;
+  let save = (repos) => {
+    for(var i = 0; i<repos.length; i++){
+      repos[i].save();
+      console.log(repos[i])
+    }
+  }
+
+  var test_array = [];
+  var repo1 = new Repo({ html_url: 'https://github.com/octocat' });
+  var repo2 = new Repo({ html_url: 'https://api.github.com/users/octocat/subscriptions' });
+
+  test_array.push(repo1);
+  test_array.push(repo2);
+
+  save(test_array);
+
+
+  module.exports.save = save;
+
+})
